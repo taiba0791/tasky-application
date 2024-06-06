0981892d0c83e3e7462b01f0a5bef9f708f5e90c
taiba0791 <shaikh.taiba2131@gmail.com>
2024-06-06 21:57:28 +0530

    d3

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+// DOM
+ 
+const taskConstents = document.querySelector(".task_contents");
+const taskModel = document.querySelector(".task_modal_body");
+
+
+
+console.log(taskConstents);
+console.log(taskModel);
